c982f1058a87a140dcbecd5240961bcea8bb541e
Walter Stones <dev@wstones.net>
2024-12-01 16:10:32 +0100

    Add splitter to MainWindow

--- editor/MainWindow.cpp
+++ editor/MainWindow.cpp
@@ -5,19 +5,36 @@
 #include <cstdio>
 #include <cassert>
 
+#include <QSplitter>
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget* parent, EntityManager* entityManager) 
 : entityManager(entityManager)
 , entityModel(new EntityModel(this, entityManager))
 , listView(new QListView(this)) {
 	assert(entityManager);
 
-	setCentralWidget(listView);
-
 	listView->setModel(entityModel);
 	listView->setSelectionRectVisible(true);
-	listView->setUniformItemSizes(true); // Improves performance
+	listView->setUniformItemSizes(true); // Improves performance if true
 	listView->show();
+
+	// TODO: add splitter for details view
+	// TODO: add these as member variables
+
+	QLabel* label = new QLabel(this);
+	label->setText("This is a test");
+
+	QSplitter* splitter = new QSplitter(this);
+	setCentralWidget(splitter); // Central widget changes layout
+	splitter->setOrientation(Qt::Vertical);
+	splitter->setStretchFactor(1, 1);
+    splitter->addWidget(listView);
+    splitter->addWidget(label);
+
 	
+	
+	// delegates
 	QItemSelectionModel* selectionModel = listView->selectionModel();
 	connect(selectionModel, &QItemSelectionModel::selectionChanged,
 			this, &MainWindow::selectionChangedSlot);
