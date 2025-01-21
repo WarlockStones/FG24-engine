04bff8c48a0f6f95e5e1518bd2c465fc863db3b3
W. Stones <warlockstones@protonmail.com>
2025-01-21 10:53:13 +0100

    Temporary fix for Windows file IO

--- engine/utils/File.cpp
+++ engine/utils/File.cpp
@@ -40,6 +40,9 @@ void FormatFilePath(char* s, std::size_t size) {
 #endif
 }
 
+// TODO: Do not use fseek() and ftell() to compute the size of a regular file
+// https://wiki.sei.cmu.edu/confluence/display/c/FIO19-C.+Do+not+use+fseek%28%29+and+ftell%28%29+to+compute+the+size+of+a+regular+file
+// TODO: Create one method for Windows and another for Linux
 const char* LoadTextFile(const char* path) {
 	// TODO: Measure lambda vs static function
 	auto Check = [path](bool failCondition, const char* functionName) {
@@ -61,7 +64,7 @@ const char* LoadTextFile(const char* path) {
 	std::memcpy(p, path, sizeof(path));
 	FormatFilePath(p, sizeof(path));
 
-	FileStream file(path, "r");
+	FileStream file(path, "rb"); // t, text-mode converts windows CRLF to LF.
 	if (Check(file.ptr == nullptr, "fopen()")) {
 		return nullptr;
 	}
@@ -86,19 +89,15 @@ const char* LoadTextFile(const char* path) {
 	std::size_t textBuffSize = static_cast<std::size_t>(fileSize + 1);
 	char* text = new char[textBuffSize]; // TODO: Fix memory leak...
 
-	// TODO: Read count is wrong when not using UNIX formatted file
 	std::size_t readCount = std::fread(
 		static_cast<char*>(text), sizeof(char), static_cast<std::size_t>(fileSize), file.ptr);
 	if (Check(readCount != static_cast<std::size_t>(fileSize), "fread()")) {
-
 		std::ferror(file.ptr);
 		std::fprintf(stderr, "Read count: %zd is not file size %I32d\n", readCount, fileSize);
 		return nullptr;
 	}
 
 	text[fileSize] = '\0';
-
-	// std::printf("Successfully read file of size %I32d\n", fileSize);
 
 	// TODO: Construct the object in return value or move? Or just use unique_ptr?
 	return text;
